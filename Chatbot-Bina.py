b4b1373fa708c1e9a6682e3767ccf6e052d0ca32
Gabriel Assunção <gabriel.a27@aluno.ifsc.edu.br>
2022-09-20 19:38:16 +0000

    chat-bot projeto

--- Chatbot-Bina.py
+++ Chatbot-Bina.py
@@ -1,25 +1,107 @@
 from os import getenv
 from dotenv import load_dotenv
 import discord
+from discord.ext import commands
+from random import choice
 load_dotenv()
-
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
-
-        if message.content.startswith('!hello'):
-            await message.reply('Hello!', mention_author=True)
-
 
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(intents=intents, command_prefix='')
 
-client = MyClient(intents=intents)
-client.run(getenv("DISCORD_TOKEN"))
+# Dicionário com as definições da máquina de estados do jogo.
+estados = {
+    0: {
+        'frases': ['Digite "iniciar" para começar o jogo e "reiniciar" caso queira voltar tudo do início.'],
+        'proximos_estados': {
+            'iniciar': 1
+        }
+    },
+    1: {
+        'frases': ['Olá!', 'Tudo bem, como vai?'],
+        'proximos_estados': {
+            'sim': 2,
+            'não': 3
+        }
+    },
+    2: {
+        'frases': ['Era uma vez...', 'E lá de volta outra vez...'],
+        'proximos_estados': {
+            'não': 3
+        }
+    },
+    3: {
+        'frases': ['Fim do jogo!', 'Parabéns!'],
+        'proximos_estados': {
+            'reiniciar': 1
+        }
+    }
+}
+
+# Dicionário com os estados correntes de cada jogador.
+partidas = {}
+
+
+@bot.event
+async def on_ready():
+    print('Bot is ready.')
+
+
+@bot.event
+async def on_message(msg):
+    # Verificar se a mensagem não tem o próprio bot como autor.
+    if msg.author.id == msg.channel.me.id:
+        return
+
+    # Verificar se o jogador ainda não começou a partida,
+    # o que significa que precisa colocá-lo no estado zero (0).
+    if msg.author.id not in partidas:
+        partidas[msg.author.id] = 0
+
+    # Em ordem de operação:
+    # 0) Obter o ID do jogador:
+    #    msg.author.id
+    # 1) Obter o estado atual do jogador:
+    #    partidas[msg.author.id]
+    # 2) Obter a definição completa desse estado:
+    #    estados[partidas[msg.author.id]]
+    # 3) Filtrar desse estado apenas a lista de próximos estados:
+    #    estados[partidas[msg.author.id]]['proximos_estados']
+    # 4) Filtrar dessa lista as chaves (frases) quem levam a próximos estados:
+    #    estados[partidas[msg.author.id]]['proximos_estados'].keys()
+    # 5) Verificar se a frase do usuário está na lista de chaves (frases) do estado:
+    if msg.content in estados[partidas[msg.author.id]]['proximos_estados'].keys():
+        #
+        # 6) Atualizar o estado do jogador, fazendo-o avançar no jogo:
+        partidas[msg.author.id] = estados[partidas[msg.author.id]]['proximos_estados'][msg.content]
+        #
+        # 7) Enviar para o jogador a mensagem do estado (já atualizado)
+        #
+        # Em ordem de operação:
+        # 7.0) Obter o ID do jogador:
+        #    msg.author.id
+        # 7.1) Obter o estado atual do jogador:
+        #    partidas[msg.author.id]
+        # 7.2) Obter a definição completa desse estado:
+        #    estados[partidas[msg.author.id]]
+        # 7.3) Filtrar desse estado apenas a lista de frases:
+        #    estados[partidas[msg.author.id]]['frases']
+        # 7.4) Sortear uma frase dessa lista:
+        #   choice(estados[partidas[msg.author.id]]['frases'])
+        await msg.channel.send(choice(estados[partidas[msg.author.id]]['frases']))
+    #
+    # Caso contrário, avisar que a mensagem não avança no jogo
+    else:
+        #
+        # Se o jogador está no primeiro estado...
+        if partidas[msg.author.id] == 0:
+            #
+            # ...ajudar com uma dica:
+            await msg.channel.send(choice(estados[partidas[msg.author.id]]['frases']))
+        else:
+            #
+            # Nos estados seguintes, a resposta padrão de HAL:
+            await msg.channel.send('I\'m sorry Dave, I\'m afraid I can\'t do that.')
+
+
+bot.run(getenv('DISCORD_TOKEN'))
