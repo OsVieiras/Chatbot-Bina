bb526309a444d6131392213bdd0a0be8f0c61beb
Gabriel Assunção <gabriel.a27@aluno.ifsc.edu.br>
2022-10-04 19:50:02 +0000

    Colocando imagens e inventário no código

--- definições.py
+++ definições.py
@@ -1,10 +1,15 @@
+frases = {
+    'inventario_insuficiente': 'Sem os recursos necessários para avançar.',
+    'erro': 'Desculpe, não entendi.'
+}
+
 estados = {
     0: {
         'frases': ['Digite "iniciar" para começar o jogo e "reiniciar" caso queira voltar ao início.'],
         'proximos_estados': {
             '[iI]nicia(r)*': 1
         },
-        'tempo_limite': 0
+        
     },
     1: {
         'frases': ['Olá! Eu sou Bina seu robô assistente. Você foi "sugado" para dentro do seu computador enquanto trabalhava em um código. Minha missão é ajudá-lo a sair de dentro da máquina e para isso você terá 3 opções de saída: impressora, monitor ou alto-falante, mas primeiro preciso saber como você foi sugado. Para me responder você terá sempre de fazer conversões decimais-binárias, pois meu interpretador está danificado. A resposta deverá ter 8 bits (00000000) sendo ela o respectivo valor em binário da opção (X) escolhida. OPÇÃO (10) = teclado, OPÇÃO (15) = mouse, OPCÃO (20) = microfone.'],
@@ -14,7 +19,7 @@ estados = {
             '00010100': 4,
             '[rR]einicia(r)*': 1 
         },
-        'tempo_limite': 0
+        'inventario': {}
     },
     2: {
         'frases': ['Certo, como você foi sugado pro computador pelo teclado, eu recomendo sair do PC pela impressora, se desejar seguir esse rumo, OPCÃO (12) = sair pela impressora, caso não queira "reiniciar".'],
@@ -22,7 +27,7 @@ estados = {
             '00001100': 5,
             '[rR]einicia(r)*': 1
         },
-        'tempo_limite': 0
+        'inventario': {}
     },
     3: {
         'frases': ['Certo, como você foi sugado pro computador pelo mouse, eu recomendo sair do PC pelo monitor, se desejar seguir esse rumo, OPÇÃO (19) = sair pelo monitor, caso não queira "reiniciar".'],
@@ -30,7 +35,7 @@ estados = {
             '00010011': 6,
             '[rR]einicia(r)*': 1
         },
-        'tempo_limite': 0       
+        'inventario': {}       
     },
     4: {
         'frases': ['Certo, como você foi sugado pro computador pelo microfone, eu recomendo sair do PC pelo alto-falante, se desejar seguir esse rumo, OPÇÃO (21) = sair pelo alto-falante, caso não queira "reiniciar".'],
@@ -38,7 +43,7 @@ estados = {
             '00010101': 7,
             '[rR]einicia(r)*': 1
         },
-        'tempo_limite': 0
+        'inventario': {}
     },
     5: {
         'frases': ['Certo! Seguiremos o nosso caminho em direção a impressora, por aqui nós enfrentaremos alguns problemas. Nosso primeiro desafio será enfrentrar um trânsito de bits que estão seguindo rumo ao processador, temos que decodificar algumas sequências binárias que por ventura vierem em nossa direção, assim, nenhum dado será perdido por não ter conseguido chegar ao processador. Devemos fazer isso para não corrermos o risco de ser enviados para a assitência técnica por estar apresentando problemas, e acabar sendo formatados/apagados para sempre. OPÇÃO (27) = seguir.'],
@@ -46,7 +51,7 @@ estados = {
             '00011011': 8,
             '[rR]einicia(r)*': 1
         },
-        'tempo_limite': 0
+        'inventario': {}
     },
 
     6: {
@@ -55,7 +60,7 @@ estados = {
             '00011011': 8,
             '[rR]einicia(r)*': 1
         },
-        'tempo_limite': 0
+        'inventario': {}
     },
     
     7: {
@@ -64,7 +69,7 @@ estados = {
             '00011011': 8,
             '[rR]einicia(r)*': 1
         },
-        'tempo_limite': 0
+        'inventario': {}
     },
 
     8: {
@@ -73,7 +78,7 @@ estados = {
             '20': 9,
             '[rR]einicia(r)*': 1
         },
-        'tempo_limite': 0
+        'inventario': {}
     },
 
     9: {
@@ -82,7 +87,7 @@ estados = {
             '11000000.10101000.00100100.00001111': 10,
             '[rR]einicia(r)*': 1
         },
-        'tempo_limite': 0
+        'inventario': {}
     },
 
     10: {
@@ -90,7 +95,7 @@ estados = {
         'proximos_estados': {
             '[rR]einicia(r)*': 1
         },
-        'tempo_limite': 0
+        'inventario': {}
     },
 
 }
